8850fa9b6cd051c7162a5311faf64be65447579d
Sneara0 <snearaparvin.cse@gmail.com>
2025-11-02 10:36:22 +0600

    first commit

---
+++ MO2V01.js
@@ -0,0 +1,49 @@
+
+
+    
+
+//const obj={};
+
+    //obj [course2] = {courseId:"level2"},
+    //obj [course1] = {courseId:"level21"},
+    //console.log(obj);
+
+
+
+
+
+
+    //nextLevel:{courseId:"Level2"},
+    //"Programming Hero":{courseId:"Level1"},
+
+//console.log(obj.nextLevel);
+//console.log(obj["Programming Hero"])
+//const  course1 = {name:"Programming Hero"};
+//const course2 ={name:"Next Level Web Development"};
+
+const courses=[
+    ["programming Hero","Level1"],
+    ["Next level","level2"]
+];
+
+
+
+const map=new Map(courses);
+//map.set(false,"Programming Hero");
+//map.set(course1,{courseId:"level1"});
+//map.set(course2,{courseId:"level2"});
+
+//console.log(map.size);
+console.log(map)
+
+//map.forEach((value,key)=>console.log('key:',key,'value:',value))
+//map.forEach((value,key)=>(key.name="Shohoz Shorol Simple"+key.name))
+//console.log(map);
+
+//console.log([...map.keys()])
+//console.log([...map.values()])
+
+//for (let key of map.keys()){
+    //key.name="Shohoz Shorol Simple "+key.name;
+//}
+//console.log(map.entries());
