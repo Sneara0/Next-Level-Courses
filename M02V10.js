c28862827cca584895245050c6d68d9fd8bcf20d
Sneara0 <snearaparvin.cse@gmail.com>
2025-11-05 11:18:17 +0600

    Add M023V01.js

---
+++ M02V10.js
@@ -0,0 +1,6 @@
+//* Binning (Resampling) time series data
+const events=[
+    {id:1,event:"click",timestamp:"2023-06-01T10:15:00Z"},
+    {id:2,event:"scroll",timestamp:"2023-06-01T10:20:00Z"},
+    {id:3,event:"click",timestamp:"2023-06-01T11:05:00Z"},
+]
---
+++ M03V01.js
@@ -0,0 +1,27 @@
+//*Stateless vs Statefull 
+//const counter=(amount)=>{
+    //et count=0;
+   //count=count+amount;
+    //return count;
+
+//} 
+//console.log( counter(3));
+//console.log( counter(2))
+
+const counter={
+    count:0,
+    add (amount){
+        this.count=this.count+amount;
+        return this.count;
+
+    },
+    print(){
+        console.log(this.count);
+
+    }
+}
+counter.add(3);
+counter.add(2);
+
+
+counter.print();
